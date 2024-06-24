1af02f95b2f772b7a5b66444fafb0645090f448e
AntonTapper <ant.tapper@gmail.com>
2024-06-24 15:57:04 +0200

    fix

--- data/index.ts
+++ data/index.ts
@@ -99,7 +99,7 @@ export const navItems = [
       des: "Promptopia is an open-source AI prompting tool for modern world to discover, create, and share creative prompts.",
       img: "/projekt-3.png",
       iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/mongodb.svg"],
-      link: "https://github.com/adrianhajdin/iphone",
+      link: "https://promptopia-amber-nine.vercel.app/",
     },
   ];
   
